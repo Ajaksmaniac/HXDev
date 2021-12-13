8e91609b215719e46f5d0a3d3fc41a0f336705f8
aleksandar-jovanov <aleksandar.jovanov@eng.it>
2021-12-13 18:59:29 +0100

    add dummy project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,22 +1,37 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import ProjectCard from "../components/ProjectCard";
 
 const Projects = () => {
   return (
-    <div className="row">
-      <div className="col-sm-4">
-        <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
+    <Container className="mt-4 ">
+      <div className="row">
+        <div className="col-sm-4 ">
+          <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
+        </div>
+        <div className="col-sm-4">
+          <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
+        </div>
+        <div className="col-sm-4">
+          <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
+        </div>
+        <div className="col-sm-4">
+          <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
+        </div>
+        <div className="col-sm-4">
+          <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
+        </div>
+        <div className="col-sm-4">
+          <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
+        </div>
+        <div className="col-sm-4">
+          <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
+        </div>
+        <div className="col-sm-4">
+          <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
+        </div>
       </div>
-      <div className="col-sm-4">
-        <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
-      </div>
-      <div className="col-sm-4">
-        <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
-      </div>
-      <div className="col-sm-4">
-        <ProjectCard name="TEST" description="TEST" link="TEST" image="asd" />
-      </div>
-    </div>
+    </Container>
   );
 };
 
